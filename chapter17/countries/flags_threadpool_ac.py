b058b7168123a14ac5f004898b09b0bb731b12c8
avery <wuqize5109@qq.com>
2017-05-21 14:25:08 +0800

    第17 章 future 并发

---
+++ chapter17/countries/flags_threadpool_ac.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+
+from concurrent import futures
+
+from flags import save_flag, get_flag, show, main
+
+MAX_WORKES = 20
+
+def download_one(cc):
+    image = get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower()+'gif')
+    return cc
+
+def download_many(cc_list):
+    cc_list = cc_list[:5]
+    with futures.ThreadPoolExecutor(max_workers=5) as executor:
+        to_do = []
+        for cc in sorted(cc_list):
+            future = executor.submit(download_one, cc)
+            to_do.append(future)
+            msg = 'Scheduled for {}: {}'
+            print(msg.format(cc, future), flush=True)
+
+        results = []
+        for future in futures.as_completed(to_do):
+            res = future.result()
+            msg = '{} result: {!r}'
+            print(msg.format(future, res))
+            results.append(res)
+
+        res = executor.map(download_one, sorted(cc_list))
+    return len(list(res))
+
+if __name__ == "__main__":
+    main(download_many)
